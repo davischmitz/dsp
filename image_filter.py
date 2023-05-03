383449d73358b20dd095f699d8798fcb42062789
Davi Schmitz <davi.schmitz@sap.com>
2023-05-02 21:36:59 -0300

    Add image filter sample

--- image_filter.py
+++ image_filter.py
@@ -9,13 +9,14 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-def noise_generator (noise_type,image):
+
+def noise_generator(noise_type, image):
     """
     Generate noise to a given Image based on required noise type
-    
+
     Input parameters:
         image: ndarray (input image data. It will be converted to float)
-        
+
         noise_type: string
             'gauss'        Gaussian-distrituion based noise
             'poission'     Poission-distribution based noise
@@ -23,14 +24,14 @@ def noise_generator (noise_type,image):
             'speckle'      Multiplicative noise using out = image + n*image
                            where n is uniform noise with specified mean & variance
     """
-    row,col,ch= image.shape
-    if noise_type == "gauss":    
+    row, col, ch = image.shape
+    if noise_type == "gauss":
         mean = 0.0
         var = 0.01
         sigma = var**0.5
         gauss = np.array(image.shape)
-        gauss = np.random.normal(mean,sigma,(row,col,ch))
-        gauss = gauss.reshape(row,col,ch)
+        gauss = np.random.normal(mean, sigma, (row, col, ch))
+        gauss = gauss.reshape(row, col, ch)
         noisy = image + gauss
         return noisy.astype('uint8')
     elif noise_type == "s&p":
@@ -40,12 +41,12 @@ def noise_generator (noise_type,image):
         # Generate Salt '1' noise
         num_salt = np.ceil(amount * image.size * s_vs_p)
         coords = [np.random.randint(0, i - 1, int(num_salt))
-              for i in image.shape]
+                  for i in image.shape]
         out[coords] = 255
         # Generate Pepper '0' noise
-        num_pepper = np.ceil(amount* image.size * (1. - s_vs_p))
+        num_pepper = np.ceil(amount * image.size * (1. - s_vs_p))
         coords = [np.random.randint(0, i - 1, int(num_pepper))
-              for i in image.shape]
+                  for i in image.shape]
         out[coords] = 0
         return out
     elif noise_type == "poisson":
@@ -53,41 +54,38 @@ def noise_generator (noise_type,image):
         vals = 2 ** np.ceil(np.log2(vals))
         noisy = np.random.poisson(image * vals) / float(vals)
         return noisy
-    elif noise_type =="speckle":
-        gauss = np.random.randn(row,col,ch)
-        gauss = gauss.reshape(row,col,ch)        
+    elif noise_type == "speckle":
+        gauss = np.random.randn(row, col, ch)
+        gauss = gauss.reshape(row, col, ch)
         noisy = image + image * gauss
         return noisy
     else:
         return image
 
 
-img = cv2.imread('opencv-logo.png')
-#img = cv2.imread("lena.jpg")
+img = cv2.imread("lena.jpg")
 
 
-#Convolução
-#kernel = np.ones((5,5),np.float32)/25
-#dst = cv2.filter2D(img,-1,kernel)
+# Convolução
+kernel = np.ones((5, 5), np.float32)/25
+dst = cv2.filter2D(img, -1, kernel)
 
-#Media
-#blur = cv2.blur(img,(5,5))
+# Media
+# blur = cv2.blur(img, (10, 10))
+# dst = blur
 
-#Gaussiano
-#blur = cv2.GaussianBlur(img,(5,5),0)
-#dst = blur
+# Gaussiano
+# blur = cv2.GaussianBlur(img, (5, 5), 0)
+# dst = blur
 
-#Mediana
-img = noise_generator('s&p',img)
-median = cv2.medianBlur(img,5)
-dst = median
+# # Mediana
+# img = noise_generator('s&p', img)
+# median = cv2.medianBlur(img, 5)
+# dst = median
 
 
-plt.subplot(121),plt.imshow(img),plt.title('Original')
+plt.subplot(121), plt.imshow(img), plt.title('Original')
 plt.xticks([]), plt.yticks([])
-plt.subplot(122),plt.imshow(dst),plt.title('Blurred')
+plt.subplot(122), plt.imshow(dst), plt.title('Blurred')
 plt.xticks([]), plt.yticks([])
 plt.show()
-
-
-
